28699a7621629c54dbe9b13bbf413886a2680c90
nhungnoon <43654499+nhungnoon@users.noreply.github.com>
2018-10-01 19:10:06 -0400

    Cleaning + turn variables to numeric

--- Analysis.R
+++ Analysis.R
@@ -1,1 +1,24 @@
 #Analysis
+
+#Summary of the data
+fifa <- read_csv("~/Desktop/fifa.csv")
+summary(fifa)
+str(fifa)
+
+**The data have 41 variables. The data describes the players' ages, nationalities, clubs, wages, and other characteristics and potentials of a soccer players. Each characteristic including dribbling, volley, or visions describe specific ability of each player and is scored by the computer.**
+
+```{r, echo = TRUE}
+summary(fifa$Age)
+summary(fifa$Overall)
+summary(fifa$Potential)
+summary(fifa$Value..in.million.)
+```
+
+```{r, echo = TRUE}
+fifa$Vision <- as.numeric(fifa$Vision)
+fifa$Penalties <- as.numeric(fifa$Penalties)
+fifa$Acceleration <- as.numeric(fifa$Acceleration)
+fifa$Strength <- as.numeric(fifa$Strength)
+fifa$Stamina.enduration. <- as.numeric(fifa$Stamina.enduration.)
+fifa$Finishing <- as.numeric(fifa$Finishing)
+```
