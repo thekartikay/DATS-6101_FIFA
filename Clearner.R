341eb85ca3880cdc2eb95c8a380190dfaa2a0216
nhungnoon <43654499+nhungnoon@users.noreply.github.com>
2018-10-01 19:11:38 -0400

    Turn variable into numeric

--- Clearner.R
+++ Clearner.R
@@ -1,1 +1,11 @@
-Cleaning file
+# Cleaning file
+
+
+```{r, echo = TRUE}
+fifa$Vision <- as.numeric(fifa$Vision)
+fifa$Penalties <- as.numeric(fifa$Penalties)
+fifa$Acceleration <- as.numeric(fifa$Acceleration)
+fifa$Strength <- as.numeric(fifa$Strength)
+fifa$Stamina.enduration. <- as.numeric(fifa$Stamina.enduration.)
+fifa$Finishing <- as.numeric(fifa$Finishing)
+```
