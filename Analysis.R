b21008e8f07a3d3124cb8c6aae1d655e628a80b4
nhungnoon <43654499+nhungnoon@users.noreply.github.com>
2018-10-01 19:10:59 -0400

    Update Analysis.R

--- Analysis.R
+++ Analysis.R
@@ -14,11 +14,3 @@ summary(fifa$Potential)
 summary(fifa$Value..in.million.)
 ```
 
-```{r, echo = TRUE}
-fifa$Vision <- as.numeric(fifa$Vision)
-fifa$Penalties <- as.numeric(fifa$Penalties)
-fifa$Acceleration <- as.numeric(fifa$Acceleration)
-fifa$Strength <- as.numeric(fifa$Strength)
-fifa$Stamina.enduration. <- as.numeric(fifa$Stamina.enduration.)
-fifa$Finishing <- as.numeric(fifa$Finishing)
-```
