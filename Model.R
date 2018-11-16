60023e5a205938cad9658c64688dac4cfb91cf13
nhungnoon <43654499+nhungnoon@users.noreply.github.com>
2018-11-16 13:25:08 -0500

    Adding goal keeper model

--- Model.R
+++ Model.R
@@ -34,3 +34,114 @@ for (kv in 2:11) {
   print( paste("total accuracy =  ",round( (Cross$prop.tbl[1,1] + Cross$prop.tbl[2,2])*100, 2),"%"   ) )
 }
 #The best value of k is 6-7, this k value yields a good accuracy of 93.14%
+
+
+##Goal Keeper
+
+
+```{r, echo = F}
+#this is coder's comment
+library(readr)
+setwd("/Users/NN/Desktop/DATS 6101")
+fifa <- read.csv("~/Desktop/cleanedfifa.csv")
+
+#Here are my variables of interests
+selectedfifa <-  fifa[,c("Age", "Strength","Stamina","Finishing","Penalties","Vision","Acceleration","Player.Mentality", "Heading.accuracy","Sprint.speed","Volleys","Balance","Composure")]
+
+#I create a variable called y that is the binary variable 
+selectedfifa$y <- ifelse(selectedfifa$Player.Mentality == "keeper",TRUE,FALSE)
+selectedfifa$y = factor(selectedfifa$y)
+selectedfifa <- selectedfifa[-c(8)]
+
+```
+
+
+```{r, echo = F,warning = F}
+#this is coder's comment
+loadPkg("leaps")
+```
+
+```{r, echo = T,warning = F}
+#this is coder's comment
+reg.leaps <- regsubsets(y~., data = selectedfifa, nbest = 1, method = "exhaustive") 
+plot(reg.leaps, scale = "adjr2", main = "Adjusted R^2")
+plot(reg.leaps, scale = "bic", main = "BIC")
+plot(reg.leaps, scale = "Cp", main = "Cp")
+```
+
+#Based on the Adjusted R2, the model with the adjust R2 of 0.79 with: Age, Stamina, Heading.accuracy, and Balance
+
+```{r, echo = F, warning = F}
+#this is coder's comment
+loadPkg("bestglm")
+```
+
+
+```{r, echo = T, warning = F}
+#this is coder's comment
+res.bestglm <- bestglm(Xy = selectedfifa, family = binomial,
+                       IC = "BIC",                 # Information criteria for
+                       method = "exhaustive")
+summary(res.bestglm)
+res.bestglm$BestModel
+```
+
+
+```{r, echo = F, warning = F}
+#this is coder's comment
+detach("package:bestglm", unload = T) 
+```
+
+#Here I createed another variable called goal keeper to turn y into numeric of 1 or 0
+
+```{r, echo = T}
+#this is coder's comment
+selectedfifa$goalkeeper <- ifelse(selectedfifa$y == "FALSE", 0, 1)
+selectedfifa$goalkeeper = factor(selectedfifa$goalkeeper)
+
+#create a logit regression 
+lmgoalkeeper <- glm(goalkeeper~ Age + Stamina+ Heading.accuracy + Balance, data = selectedfifa, family = "binomial")
+summary(lmgoalkeeper)
+```
+
+
+#a) Hoslem and Lemeshow goodness-of-fit (GOF) test.
+```{r, echo = T}
+#this is coder's comment
+loadPkg("ResourceSelection")
+hoslem.test(selectedfifa$goalkeeper, fitted(lmgoalkeeper)) 
+detach("package:ResourceSelection", unload = T) 
+```
+#pvalue is small => significant
+
+```{r, echo = T}
+#this is coder's comment
+loadPkg("pROC")
+prob=predict(lmgoalkeeper, type = c("response"))
+selectedfifa$prob=prob
+h <- roc(y~prob, data=selectedfifa)
+auc(h)
+plot(h)
+detach("package:pROC", unload = T) 
+```
+#the area is 1, which is very good.
+
+#c) The McFadden statistics.
+```{r, echo = F}
+#this is coder's comment
+loadPkg("pscl") 
+```
+
+```{r, echo = T}
+pR2(lmgoalkeeper)
+```
+
+#the McFadden value 0.996, which means the model explains about 99.6% of the probability of being a goal keeper
+
+```{r, echo = F}
+#this is coder's comment
+detach("package:pscl", unload = T)
+```
+
+
+
