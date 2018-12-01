c4f2fe2553b2c173364b0113bd016fb5ee5b9338
nhungnoon <43654499+nhungnoon@users.noreply.github.com>
2018-12-01 14:24:14 -0500

    Reorganize codes according to slide

--- Model.R
+++ Model.R
@@ -1,40 +1,12 @@
-#model
+# DATS 6101 - 2nd project - Group 5
 
-#Attack
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+loadPkg = function(x) { if (!require(x,character.only=T, quietly =T)) { install.packages(x,dep=T,repos="http://cran.us.r-project.org"); if(!require(x,character.only=T)) stop("Package not found") } }
+```
 
-#Read the file
-data <- data.read('cleanedFifa.csv')
-data_sub <- data[,c('Overall', 'Potential', 'Balance', 'Ball.control', 'Composure', 
-                    'Crossing', 'Curve', 'Dribbling', 'Finishing', 'Free.kick.accuracy', 
-                    'Marking', 'Penalties', 'Positioning', 'Reactions', 'Short.passing', 
-                    'Shot.power','Sliding.tackle', 'Sprint.speed', 'Stamina', 'Standing.tackle', 
-                    'Strength', 'Vision', 'Volleys', 'Player.Mentality')]
-#Creating the target variable
-data_sub$target[data$Player.Mentality=='attack'] <- 1
-data_sub$target[data$Player.Mentality!='attack'] <- 0
 
-#Engineering the data,i.e, feature scaling and test-train split
-randomize <- sample(2, nrow(data_sub), replace=TRUE, prob=c(0.8, 0.2))
-scaled_fifa <- as.data.frame(scale(data_sub[1:23], center = TRUE, scale = TRUE))
-scaled_fifa$target <- data_sub$target
-X_train <- scaled_fifa[randomize==1,1:ncol(scaled_fifa)-1]
-X_test <- scaled_fifa[randomize==2,1:ncol(scaled_fifa)-1]
-y_train <- scaled_fifa[randomize==1,ncol(scaled_fifa)]
-y_test <- scaled_fifa[randomize==2,ncol(scaled_fifa)]
-
-#Create KNN classification model
-library("FNN")
-pred <- knn(train = X_train[1:2], test = X_test[1:2], cl=y_train, k=6)
-
-#Parameter tuning to figure out the best value of k
-for (kv in 2:11) {
-  print(paste("******* k = ",kv," ***************************" ))
-  pred <- knn(train = X_train, test = X_test, cl=y_train, k=kv)
-  Cross <- CrossTable(y_test, pred, prop.chisq = FALSE)
-  print( paste("total accuracy =  ",round( (Cross$prop.tbl[1,1] + Cross$prop.tbl[2,2])*100, 2),"%"   ) )
-}
-#The best value of k is 6-7, this k value yields a good accuracy of 93.14%
-
+#Slide 4
 
 ##Defense
 
@@ -106,165 +78,53 @@ for (k in 1:15) {
 ```
 #k=6 or 7 have the highest accuracy, at 95%
 
-##Goal Keeper
+
+##Mid-field
 
 
-```{r, echo = F}
-#this is coder's comment
-library(readr)
-setwd("/Users/NN/Desktop/DATS 6101")
-fifa <- read.csv("~/Desktop/cleanedfifa.csv")
+#Read the file
+data <- data.read('cleanedFifa.csv')
+data_sub <- data[,c('Overall', 'Potential', 'Balance', 'Ball.control', 'Composure', 
+                    'Crossing', 'Curve', 'Dribbling', 'Finishing', 'Free.kick.accuracy', 
+                    'Marking', 'Penalties', 'Positioning', 'Reactions', 'Short.passing', 
+                    'Shot.power','Sliding.tackle', 'Sprint.speed', 'Stamina', 'Standing.tackle', 
+                    'Strength', 'Vision', 'Volleys', 'Player.Mentality')]
+#Creating the target variable
+data_sub$target[data$Player.Mentality=='mid'] <- 1
+data_sub$target[data$Player.Mentality!='mid'] <- 0
 
-#Here are my variables of interests
-selectedfifa <-  fifa[,c("Age", "Strength","Stamina","Finishing","Penalties","Vision","Acceleration","Player.Mentality", "Heading.accuracy","Sprint.speed","Volleys","Balance","Composure")]
+#Engineering the data,i.e, feature scaling and test-train split
+randomize <- sample(2, nrow(data_sub), replace=TRUE, prob=c(0.8, 0.2))
+scaled_fifa <- as.data.frame(scale(data_sub[1:23], center = TRUE, scale = TRUE))
+scaled_fifa$target <- data_sub$target
+X_train <- scaled_fifa[randomize==1,1:ncol(scaled_fifa)-1]
+X_test <- scaled_fifa[randomize==2,1:ncol(scaled_fifa)-1]
+y_train <- scaled_fifa[randomize==1,ncol(scaled_fifa)]
+y_test <- scaled_fifa[randomize==2,ncol(scaled_fifa)]
 
-#I create a variable called y that is the binary variable 
-selectedfifa$y <- ifelse(selectedfifa$Player.Mentality == "keeper",TRUE,FALSE)
-selectedfifa$y = factor(selectedfifa$y)
-selectedfifa <- selectedfifa[-c(8)]
+#Create KNN classification model
+library("FNN")
+pred <- knn(train = X_train[1:2], test = X_test[1:2], cl=y_train, k=6)
 
-```
-
-```{r, echo = T,warning = F}
-#this is coder's comment
-reg.leaps <- regsubsets(y~., data = selectedfifa, nbest = 1, method = "exhaustive") 
-plot(reg.leaps, scale = "adjr2", main = "Adjusted R^2")
-plot(reg.leaps, scale = "bic", main = "BIC")
-plot(reg.leaps, scale = "Cp", main = "Cp")
-```
-
-#Based on the Adjusted R2, the model with the adjust R2 of 0.79 with: Age, Stamina, Heading.accuracy, and Balance
-
-
-#Here I createed another variable called goal keeper to turn y into numeric of 1 or 0
-
-```{r, echo = T}
-#this is coder's comment
-selectedfifa$goalkeeper <- ifelse(selectedfifa$y == "FALSE", 0, 1)
-selectedfifa$goalkeeper = factor(selectedfifa$goalkeeper)
-
-#create a logit regression 
-lmgoalkeeper <- glm(goalkeeper~ Age + Stamina+ Heading.accuracy + Balance, data = selectedfifa, family = "binomial")
-summary(lmgoalkeeper)
-```
-
-
-
-#If I try knn selected models
-
-
-#Follow the professor's code
-
-loadPkg("FNN")
-scaled_selectedfifa <- as.data.frame(scale(selectedfifa[1:12], center = TRUE, scale = TRUE))
-set.seed(10)
-sample <- sample(2, nrow(scaled_selectedfifa), replace=TRUE, prob=c(0.8, 0.2))
-
-scaled_training <- scaled_selectedfifa[sample==1,1:12]
-scaled_test <- scaled_selectedfifa[sample==2,1:12]
-
-trainLabels <- selectedfifa[sample==1, 13]
-testLabels <- selectedfifa[sample==2, 13]
-
-pred <- knn(train =scaled_training, test = scaled_test, cl=trainLabels, k=5)
-
-
-loadPkg("gmodels")
-predCross <- CrossTable(testLabels, pred, prop.chisq = FALSE)
-
-loadPkg("class")
-set.seed(10)
-#Try k =5
-
-
-chooseK = function(k, train_set, test_set, predict1, actual){
-  pred <- knn(train = train_set, test = test_set, cl=predict1, k=k)
-  Cross <- CrossTable(actual, pred, prop.chisq = FALSE)
-  
-  tab = table(pred, actual)
-  accu = sum(tab[row(tab) == col(tab)]) / sum(tab)                        
-  cbind(k = k, accuracy = accu)
-  
+#Parameter tuning to figure out the best value of k
+for (kv in 2:11) {
+  print(paste("******* k = ",kv," ***************************" ))
+  pred <- knn(train = X_train, test = X_test, cl=y_train, k=kv)
+  Cross <- CrossTable(y_test, pred, prop.chisq = FALSE)
+  print( paste("total accuracy =  ",round( (Cross$prop.tbl[1,1] + Cross$prop.tbl[2,2])*100, 2),"%"   ) )
 }
 
-knn_different_k = sapply(seq(1,24,by =2), function(x) chooseK(x,
-                                                              train_set = scaled_training,
-                                                              test_set = scaled_test,
-                                                              predict1 = trainLabels,
-                                                              actual = testLabels))
+
+#Slide 5
 
 
-str(knn_different_k)
-
-knn_different_k = data.frame(k = knn_different_k[1,], accuracy = knn_different_k[2,])
 
 
-loadPkg("ggplot2")
-
-ggplot(knn_different_k,
-       aes(x = k, y = accuracy)) +
-  geom_line(color = "orange", size = 1.5) +
-  geom_point(size = 3)
 
 
-#After k =5, the accuracy does not change much. As such, choosing 5 features will lead to an accuracy of 0.92 or 92%. 
 
-#Testing to predict classes
-library(readr)
-fifa <- read.csv("~/Desktop/cleanedfifa.csv")
-
-selectedfifa <- fifa[-c(1,3,6,9,44)]
-
-loadPkg("FNN")
-loadPkg("gmodels")
-
-scaled_selectedfifa <- as.data.frame(scale(selectedfifa[1:39], center = TRUE, scale = TRUE))
-
-#Sample 80% as train date and 20% as test
-set.seed(10)
-sample <- sample(2, nrow(scaled_selectedfifa), replace=TRUE, prob=c(0.8, 0.2))
-
-scaled_training <- scaled_selectedfifa[sample==1,1:39]
-scaled_test <- scaled_selectedfifa[sample==2,1:39]
-
-trainLabels <- selectedfifa[sample==1, 40]
-testLabels <- selectedfifa[sample==2, 40]
-
-loadPkg("class")
-set.seed(10)
-#Try k =5
-
-
-chooseK = function(k, train_set, test_set, predict1, actual){
-  pred <- knn(train = train_set, test = test_set, cl=predict1, k=k)
-  Cross <- CrossTable(actual, pred, prop.chisq = FALSE)
-  
-  tab = table(pred, actual)
-  accu = sum(tab[row(tab) == col(tab)]) / sum(tab)                        
-  cbind(k = k, accuracy = accu)
-  
-}
-
-knn_different_k = sapply(seq(1,24,by =2), function(x) chooseK(x,
-                                                              train_set = scaled_training,
-                                                              test_set = scaled_test,
-                                                              predict1 = trainLabels,
-                                                              actual = testLabels))
-
-
-str(knn_different_k)
-
-knn_different_k = data.frame(k = knn_different_k[1,], accuracy = knn_different_k[2,])
-
-
-loadPkg("ggplot2")
-
-ggplot(knn_different_k,
-       aes(x = k, y = accuracy)) +
-  geom_line(color = "orange", size = 1.5) +
-  geom_point(size = 3)
-
-
+#Slide 6
+                         
 #Logistic Regression Model
                          
 #Analysis
@@ -446,12 +306,76 @@ data$pred_keeper <- NULL
 head(data)
 ```
 
+#Printing out the accuracy                          
 ```{r}
 accuracy = round((sum(data$Player.Mentality == data$predicted)) / nrow(data) * 100, 2)
 paste("Logistic Regression Accuracy:", accuracy, '%')
 ```
 
+#Slide 7
+
+#Create the table to check the accuracy for all position                         
 ```{r, echo=FALSE}
 library(gmodels)
 CrossTable(data$Player.Mentality, data$predicted)
 ```
+
+
+
+#Slide 8
+
+#Testing to predict classes
+library(readr)
+fifa <- read.csv("~/Desktop/cleanedfifa.csv")
+
+selectedfifa <- fifa[-c(1,3,6,9,44)]
+
+loadPkg("FNN")
+loadPkg("gmodels")
+
+scaled_selectedfifa <- as.data.frame(scale(selectedfifa[1:39], center = TRUE, scale = TRUE))
+
+#Sample 80% as train date and 20% as test
+set.seed(10)
+sample <- sample(2, nrow(scaled_selectedfifa), replace=TRUE, prob=c(0.8, 0.2))
+
+scaled_training <- scaled_selectedfifa[sample==1,1:39]
+scaled_test <- scaled_selectedfifa[sample==2,1:39]
+
+trainLabels <- selectedfifa[sample==1, 40]
+testLabels <- selectedfifa[sample==2, 40]
+
+loadPkg("class")
+set.seed(10)
+#Try k =5
+
+
+chooseK = function(k, train_set, test_set, predict1, actual){
+  pred <- knn(train = train_set, test = test_set, cl=predict1, k=k)
+  Cross <- CrossTable(actual, pred, prop.chisq = FALSE)
+  
+  tab = table(pred, actual)
+  accu = sum(tab[row(tab) == col(tab)]) / sum(tab)                        
+  cbind(k = k, accuracy = accu)
+  
+}
+
+knn_different_k = sapply(seq(1,24,by =2), function(x) chooseK(x,
+                                                              train_set = scaled_training,
+                                                              test_set = scaled_test,
+                                                              predict1 = trainLabels,
+                                                              actual = testLabels))
+
+
+str(knn_different_k)
+
+knn_different_k = data.frame(k = knn_different_k[1,], accuracy = knn_different_k[2,])
+
+
+loadPkg("ggplot2")
+
+ggplot(knn_different_k,
+       aes(x = k, y = accuracy)) +
+  geom_line(color = "orange", size = 1.5) +
+  geom_point(size = 3)
+
